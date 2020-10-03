dc235b298b5b65822eb08d6ffe21771220ee45ec
jeth318 <jesper.thornberg@me.com>
2020-10-03 02:20:38 +0200

    Remove node_module delete deploy step

--- deploy.sh
+++ deploy.sh
@@ -18,8 +18,6 @@ echo "Pulling from ${BRANCH_NAME}"
 git pull origin ${BRANCH_NAME}
 echo "Pulled successfully"
 
-echo "Removing node_modules"
-rm -rf node_modules
 echo "Reinstalling dependencies"
 npm install
 echo "Project dependencies was installed"
